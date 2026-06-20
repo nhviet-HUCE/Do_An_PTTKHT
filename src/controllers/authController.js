9f0d848e7d300e27a3503b194559dd9e2b069745
Minh <khanhminh3006@gmail.com>
2026-06-20 21:34:01 +0700

    fix bug đăng nhập đăng ký be fe

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,13 +3,12 @@ const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
 exports.login=async(req,res,next)=>{
     try {
-        const {User_name,User_password}=req.body;
-        const sql='select * from user where User_name=?';
-        const user=await db.query(sql,[User_name]);
-        if(!user) return res.status(404).json({message:'User not found'});
-
+        const {Phone_num,User_password}=req.body;
+        const sql='select * from user where phone_num=?';
+        const user=await db.query(sql,[Phone_num]);
+        if(!user) return res.status(404).json({message:'Tài khoản không tồn tại'});
         const isMatch=await bcrypt.compare(User_password,user[0].User_password);
-        if(!isMatch) return res.status(401).json({message:'Invalid credentials'});
+        if(!isMatch) return res.status(401).json({message:'Mật khẩu không đúng'});
 
         const token=jwt.sign({id:user[0].id},process.env.JWT_SECRET,{expiresIn:'1h'});
         res.json({user:user[0],token:token});
@@ -19,11 +18,11 @@ exports.login=async(req,res,next)=>{
 }
 exports.register=async(req,res,next)=>{
     try {
-        const {User_name,User_password}=req.body;
+        const {User_name,full_name,phone_number,User_password}=req.body;
         const hashedPassword=await bcrypt.hash(User_password,4);
-        const sql='insert into user (User_name,User_password) values (?,?)';
-        await db.execute(sql,[User_name,hashedPassword]);
-        res.json({message:'User registered successfully'});
+        const sql='insert into user (User_name,name,phone_num,User_password) values (?,?,?,?)';
+        await db.execute(sql,[User_name,full_name,phone_number,hashedPassword]);
+        res.json({message:'Tài khoản đã được đăng ký thành công'});
     } catch (err) {
         next(err);
     }
