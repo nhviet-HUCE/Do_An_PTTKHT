5b30416b9d2f159b0a7da1e10707de2efea61e37
NHV1et <0406969@st.huce.edu.vn>
2026-06-20 10:32:44 +0700

    1

--- app_routes.js
+++ app_routes.js
@@ -4,7 +4,7 @@ const hoa_don_route=require('./src/routes/hoa_don_routes');
 const gio_hang_route = require('./src/routes/gio_hang_routes');
 const san_pham_route = require('./src/routes/san_pham_routes');
 
-routes.use('/san_pham',san_pham_route);
-routes.use('/hoa_don',hoa_don_route);
-routes.use('/gio_hang',gio_hang_route);
+routes.use('/product',san_pham_route);
+routes.use('/invoice',hoa_don_route);
+routes.use('/cart',gio_hang_route);
 module.exports=routes;
